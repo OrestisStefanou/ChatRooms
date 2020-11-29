afd3ec537b9f9120da72f75dd641b397e3ec7a22
Orestis Stefanou <orestaras1997@outlook.com>
2020-11-29 15:36:41 +0200

    Ceasar encryption added

--- SystemMonitor/systemMonitor.go
+++ SystemMonitor/systemMonitor.go
@@ -73,14 +73,61 @@ func handleClient(conn net.Conn) {
 	}
 }
 
+func cipher(text string, direction int) string {
+	// shift -> number of letters to move to right or left
+	// offset -> size of the alphabet, in this case the plain ASCII
+	shift, offset := rune(3), rune(26)
+
+	// string->rune conversion
+	runes := []rune(text)
+
+	for index, char := range runes {
+		// Iterate over all runes, and perform substitution
+		// wherever possible. If the letter is not in the range
+		// [1 .. 25], the offset defined above is added or
+		// subtracted.
+		switch direction {
+		case -1: // encoding
+			if char >= 'a'+shift && char <= 'z' ||
+				char >= 'A'+shift && char <= 'Z' {
+				char = char - shift
+			} else if char >= 'a' && char < 'a'+shift ||
+				char >= 'A' && char < 'A'+shift {
+				char = char - shift + offset
+			}
+		case +1: // decoding
+			if char >= 'a' && char <= 'z'-shift ||
+				char >= 'A' && char <= 'Z'-shift {
+				char = char + shift
+			} else if char > 'z'-shift && char <= 'z' ||
+				char > 'Z'-shift && char <= 'Z' {
+				char = char + shift - offset
+			}
+		}
+
+		// Above `if`s handle both upper and lower case ASCII
+		// characters; anything else is returned as is (includes
+		// numbers, punctuation and space).
+		runes[index] = char
+	}
+
+	return string(runes)
+}
+
+// encode and decode provide the API for encoding and decoding text using
+// the Caesar Cipher algorithm.
+func encode(text string) string { return cipher(text, -1) }
+func decode(text string) string { return cipher(text, +1) }
+
 //Send msg
 func sendMsg(conn net.Conn, msg string) {
+	msgToSend := encode(msg) + "\n"
 	bytesSent := 0
-	msgLen, err := fmt.Fprintf(conn, msg)
+	msgLen, err := fmt.Fprintf(conn, msgToSend)
 	checkError(err)
 	bytesSent = bytesSent + msgLen
-	for bytesSent != len(msg) {
-		msgLen, err = fmt.Fprintf(conn, msg[bytesSent:])
+	for bytesSent != len(msgToSend) {
+		msgLen, err = fmt.Fprintf(conn, msgToSend[bytesSent:])
 		checkError(err)
 		bytesSent = bytesSent + msgLen
 	}
@@ -92,7 +139,7 @@ func recMsg(conn net.Conn) string {
 	if err != nil {
 		return fmt.Sprintf("Closed%s", specialString)
 	}
-	return strings.TrimSuffix(message, "\n")
+	return strings.Trim(decode(message), "\n")
 }
 
 func printYellow(text string) {
